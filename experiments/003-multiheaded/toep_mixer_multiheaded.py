e412d1c75707862b276799350d8be4ee523fe80f
blbadger <blbadger10@gmail.com>
2025-11-10 19:48:16 +0000

    minor updates

--- experiments/003-multiheaded/toep_mixer_multiheaded.py
+++ experiments/003-multiheaded/toep_mixer_multiheaded.py
@@ -271,24 +271,24 @@ if __name__ == "__main__":
     load_dotenv()
     checkpoint_root = os.getenv('CHECKPOINT_ROOT')
     data_root = os.getenv('DATA_ROOT')
-    tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_fineweb_8k")
+    tokenizer = AutoTokenizer.from_pretrained(f"{data_root}/tokenizer_stack_8k")
     tokenizer.pad_token = tokenizer.eos_token
     n_vocab = len(tokenizer)
     print("Vocab size: ", n_vocab)
 
-    tokenized_length = 512
-    dim = 256
+    tokenized_length = 1024
+    dim = 1024
     layers = 16
-    n_heads = None
+    n_heads = 4
 
     model = MLPMixer(
         n_vocab, dim, tokenized_length, layers, heads=n_heads, expanded_convs=False
     ).float()
 
-    train_path = f"{data_root}/fineweb-edu-tokenized-train-c512"
-    test_path = f"{data_root}/fineweb-edu-tokenized-test-c512"
+    train_path = f"{data_root}/stack-tokenized-train-c1024-8k"
+    test_path = f"{data_root}/stack-tokenized-test-c1024-8k"
 
-    output_dir = f"{checkpoint_root}/fineweb_flat_toep_256_n16_c512_b32x4"
+    output_dir = f"{checkpoint_root}/stack_flat_toep_1024_n16_c1024_b16x4"
     
     datasets.config.IN_MEMORY_MAX_SIZE = 50e9
     train_dataset = load_from_disk(train_path, keep_in_memory=None)
@@ -299,8 +299,9 @@ if __name__ == "__main__":
     print(model)
     training_arguments = transformers.TrainingArguments(
         num_train_epochs=2,
-        per_device_train_batch_size=32,
-        per_device_eval_batch_size=32,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        gradient_accumulation_steps=2,
         warmup_steps=500,
         eval_steps=4000,
         save_steps=8000,
